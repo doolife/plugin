d136791892fed1238bdc5a782985eb174af44846
LeeDooNam <doolife@ncsoft.com>
2019-01-08 17:16:41 +0900

    [vuejs] test

--- src/vuejs/js/index.js
+++ src/vuejs/js/index.js
@@ -3,11 +3,23 @@ import '../sass/index.scss';
 var app = new Vue({
     el:'#app',
     data:{
-        name:'이은석',
-        age:'3살',
-        month:'22개월',
-        time:'오전 2시 7분'
+        todos:[
+            {text:"삐삐"},
+            {text:"금요일에 만나요"},
+            {text:"미아"},
+        ],
+        name:"Vue",
+        smile:false,
+        iu1:"http://image.koreatimes.com/article/2018/07/17/201807171740275b1.jpg",
+        iu2:"https://i.pinimg.com/474x/75/ac/6f/75ac6fafb8083d046d6b35c9cf364106.jpg",
+        number:0
+    },
+    methods:{
+        inc:function(){
+            this.number++;
+        },
+        dec:function(){
+            this.number--;
+        }
     }
 });
-
-console.log(app.name)
