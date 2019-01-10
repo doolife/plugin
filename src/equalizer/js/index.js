cca8df99b09828e61ca62338f566fb4563269d48
LeeDooNam <doolife@ncsoft.com>
2019-01-10 09:55:04 +0900

    develop merge

--- src/equalizer/js/index.js
+++ src/equalizer/js/index.js
@@ -1,4 +1,5 @@
 import '../sass/index.scss';
+import '../media/audio.mp3';
 
 var Equalizer = (function(){
 
@@ -67,7 +68,7 @@ var eq = new Equalizer({
     parent:"#equalizer",            // 이퀄라이저 애니메이션 부모 엘리먼트
     children:"span",                // 이퀄라이저 애니메이션 자식 엘리먼트
     btn:".btn_eq",                  // 버튼 클래스
-    url:"../vendor/media/audio.mp3",       // 오디오 url
+    url:"media/audio.mp3",       // 오디오 url
     volume:0.5,                     // 볼륨
     setStr:false                    // 이퀄라이저 동작여부(true : play / false : pause)  ※ 크롬에서는 크롬 정책으로 인해 자동 재생 불가
 });
