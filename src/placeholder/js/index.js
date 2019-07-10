00c449f1643fcd52b17bd2784513dcdc4e96c120
doonam <doolife@ncits.co.kr>
2019-07-10 16:52:14 +0900

    placeholder var -> const

--- src/placeholder/js/index.js
+++ src/placeholder/js/index.js
@@ -2,14 +2,14 @@ import '../sass/index.scss';
 import 'babel-polyfill';
 import Placeholder from './page/placeholder';
 
-var placeHolder1 = new Placeholder({
+const placeHolder1 = new Placeholder({
     el:"#placeHolder1"
 });
 
-var placeHolder2 = new Placeholder({
+const placeHolder2 = new Placeholder({
     el:"#placeHolder2"
 });
 
-var placeHolder3 = new Placeholder({
+const placeHolder3 = new Placeholder({
     el:"#placeHolder3"
 });
