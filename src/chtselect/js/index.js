40b3605b18307941960cddf7e2ebd4269d385eaf
doonam <doolife@ncits.co.kr>
2020-04-13 14:37:01 +0900

    [chtselect] add

---
+++ src/chtselect/js/index.js
@@ -0,0 +1,9 @@
+import '../sass/index.scss';
+import ChtSelect from './util/cht-select';
+
+const chtselect = new ChtSelect({
+    el:"#contents",
+    scale:1.1,
+    leftX:-50,
+    rightX:50
+});
