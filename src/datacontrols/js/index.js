d8f1edb5136f4d7f4078031cb41c32c7841a36ce
doonam <doolife@ncits.co.kr>
2020-04-14 08:47:07 +0900

    [datacontrols] delete

--- src/datacontrols/js/index.js
+++
@@ -1,9 +0,0 @@
-import '../sass/index.scss';
-import Datacontrols from './page/datacontrols';
-
-
-const datacontrols = new Datacontrols({
-    el:"#datacontrols",
-    url:"https://www.kobis.or.kr",
-    key:"430156241533f1d058c603178cc3ca0e"
-});
