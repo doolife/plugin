e178cb7a657c6b04cc6734774d185b14d1cc6abb
doonam <doolife@ncits.co.kr>
2020-06-25 09:51:18 +0900

    [tabmenu] edit

--- src/tabmenu/js/page/tabmenu.js
+++ src/tabmenu/js/page/tabmenu.js
@@ -9,12 +9,10 @@ class TabMenu{
         }, opts);
 
         this.elWrap = document.querySelector(this.opts.el);
-        this.elTabWrap = this.elWrap.querySelector("[data-tab='wrap']");
-        this.elTabMenu = this.elTabWrap.querySelectorAll("[data-list]");
-        this.elConWrap = this.elWrap.querySelector("[data-con='wrap']");
-        this.elConList = this.elConWrap.querySelectorAll("[data-list]");
+        this.elTabWrap = this.elWrap.querySelector("[data-tab-wrap]");
+        this.elTabMenu = this.elWrap.querySelectorAll("[data-tab]");
+        this.elConList = this.elWrap.querySelectorAll("[data-con]");
 
-        this.selected;
         this.currId;
         this.prevId;
 
@@ -27,34 +25,26 @@ class TabMenu{
     };
 
     settings(){
-        Array.from(this.elTabMenu).forEach((value, index)=>{
-            if(index==this.opts.idx-1){
-                this.selected = this.elTabMenu[index].getAttribute("data-list");
-            }
-        });
-        this.selection(this.selected);
+        let selected = this.elTabMenu[this.opts.idx-1].getAttribute("data-tab");
+        this.selection(selected);
     }
 
     controls(){
-        this.elTabWrap.addEventListener("click", (e)=>{
-            if(e.target.nodeName == "BUTTON") {
-                if(e.target.parentNode.hasAttribute("data-disabled")) return;
-                if(e.target.parentNode.classList.contains("active")) return;
-                this.selection(e.target.parentNode.getAttribute("data-list"));
-            }
+        this.elTabWrap.addEventListener("click", evt=>{
+            this.selection(evt.target.getAttribute("data-tab"));
         });
     }
 
-    selection(name){
-        this.currId = name;
+    selection(selected){
+        this.currId = selected;
 
         if(this.prevId!=undefined){
-            this.elTabWrap.querySelector(`[data-list="${this.prevId}"]`).classList.remove("active");
-            this.elConWrap.querySelector(`[data-list="${this.prevId}"]`).classList.remove("active");
+            this.elWrap.querySelector(`[data-tab="${this.prevId}"]`).classList.remove("active");
+            this.elWrap.querySelector(`[data-con="${this.prevId}"]`).classList.remove("active");
         }
 
-        this.elTabWrap.querySelector(`[data-list="${name}"]`).classList.add("active");
-        this.elConWrap.querySelector(`[data-list="${name}"]`).classList.add("active");
+        this.elWrap.querySelector(`[data-tab="${this.currId}"]`).classList.add("active");
+        this.elWrap.querySelector(`[data-con="${this.currId}"]`).classList.add("active");
 
         if (typeof this.opts.tabCallback == "function"){
             this.opts.tabCallback(this.currId, this.prevId);
@@ -63,8 +53,8 @@ class TabMenu{
         this.prevId = this.currId;
     }
 
-    set seletedSet(name){
-        this.selection(name);
+    set seletedSet(selected){
+        this.selection(selected);
     }
 
 };
