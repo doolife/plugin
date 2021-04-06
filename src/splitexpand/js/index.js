5c088938bded3d155febf729c5ca55d0e4131ce5
doonam <doolife@ncits.co.kr>
2021-04-06 17:19:51 +0900

    [splitexpand] callback & trigger

--- src/splitexpand/js/index.js
+++ src/splitexpand/js/index.js
@@ -7,5 +7,16 @@ const splitexpand = new Splitexpand({
     resetArr:["33%", "34%", "33%"],
     over:"50%",
     down:"25%",
-    speed:500
+    speed:500,
+    enterCallback(){
+        console.log(this.currIdx, "currIdx")
+        console.log(this.prevIdx, "prevIdx")
+    },
+    leaveCallback(){
+
+    }
+});
+
+splitexpand.on("mouseLeave", ()=>{
+    console.log(splitexpand.prevIdx, "mouseLeave")
 });
--- src/splitexpand/js/util/splitexpand.js
+++ src/splitexpand/js/util/splitexpand.js
@@ -5,12 +5,21 @@ class splitexpand {
             resetArr:["33%", "34%", "33%"],
             over:"50%",
             down:"25%",
-            speed:500
+            speed:500,
+            enterCallback(){
+                // console.log(this.currIdx, "callback")
+            },
+            leaveCallback(){
+
+            }
         }, opts);
 
         this.$el = $(this.opts.el);
         this.$list = this.$el.find(".split-expand__list");
+        this.currIdx;
+        this.prevIdx;
         this.setStr = null;
+        this.overAction = true;
 
         this.init();
     };
@@ -27,21 +36,37 @@ class splitexpand {
     };
 
     overAct(evt){
+        if(!this.overAction) return;
         $.each(this.$list, (index, element)=>{
             if(element===evt.currentTarget) {
                 $(element).stop().animate({width:this.opts.over}, this.opts.speed);
+                this.currIdx = index;
+                this.$el.trigger("mouseEnter");
+                this.methodDepth("enterCallback");
+                this.prevIdx = this.currIdx;
             }else{
                 $(element).stop().animate({width:this.opts.down}, this.opts.speed);
             };
         });
-    }
+    };
 
     resetAct(){
+        if(!this.overAction) return;
         $.each(this.$list, (index, element)=>{
             this.setStr && $(element).stop().animate({width:this.opts.resetArr[index]}, this.opts.speed);    // this.setStr === true 일 경우 뒤의 값을 반환
             this.setStr || $(element).css({width:this.opts.resetArr[index]});    // this.setStr === false 일 경우 뒤의 값을 반환
         });
+        this.$el.trigger("mouseLeave");
+        this.methodDepth("leaveCallback");
         this.setStr = true;
+    };
+
+    methodDepth(funcValue){
+        if (typeof this.opts[`${funcValue}`] == "function") this.opts[`${funcValue}`].call(this);
+    };
+
+    on(event, func){
+        this.$el.on(event, func);
     };
 };
 
