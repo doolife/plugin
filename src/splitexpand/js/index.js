46b3c3624649ef3f64ad3d8d17ff64170d97e5ef
doonam <doolife@ncsoft.com>
2022-06-17 11:06:55 +0900

    [splitexpand] click controls add

--- src/splitexpand/js/index.js
+++ src/splitexpand/js/index.js
@@ -7,16 +7,34 @@ const splitexpand = new Splitexpand({
     resetArr:["33%", "34%", "33%"],
     over:"50%",
     down:"25%",
+    maxOver:"60%",
+    minDown:"20%",
+    clickEvent:false,
     speed:500,
     enterCallback(){
-        console.log(this.currIdx, "currIdx")
-        console.log(this.prevIdx, "prevIdx")
+        // console.log(this.currIdx, "currIdx")
+        // console.log(this.prevIdx, "prevIdx")
     },
     leaveCallback(){
 
+    },
+    clickCallback(){
+        // console.log(this.currIdx, "currIdx")
+    },
+    enterCallbackMax(){
+        // console.log(this.currIdx, "currIdx")
     }
 });
 
 splitexpand.on("mouseLeave", ()=>{
-    console.log(splitexpand.prevIdx, "mouseLeave")
+    console.log(splitexpand.currIdx, "mouseLeave")
+});
+
+splitexpand.on("mouseClick", ()=>{
+    console.log(splitexpand.currIdx, "mouseClick")
+});
+
+splitexpand.on("mouseEnterMax", ()=>{
+    // $(`.split-expand__list--wrap${splitexpand.currIdx+1}`)
+    console.log(splitexpand.currIdx, "mouseEnterMax")
 });
--- src/splitexpand/js/util/splitexpand.js
+++ src/splitexpand/js/util/splitexpand.js
@@ -5,21 +5,30 @@ class splitexpand {
             resetArr:["33%", "34%", "33%"],
             over:"50%",
             down:"25%",
-            speed:500,
+            maxOver:"60%",
+            minDown:"20%",
             enterCallback(){
-                // console.log(this.currIdx, "callback")
+                // console.log(this.currIdx, "currIdx")
+                // console.log(this.prevIdx, "prevIdx")
             },
             leaveCallback(){
 
+            },
+            clickCallback(){
+                // console.log(this.currIdx, "currIdx")
+            },
+            enterCallbackMax(){
+                // console.log(this.currIdx, "currIdx")
             }
         }, opts);
 
         this.$el = $(this.opts.el);
-        this.$list = this.$el.find(".split-expand__list");
+        this.$list = this.$el.find("[data-split]");
         this.currIdx;
         this.prevIdx;
         this.setStr = null;
         this.overAction = true;
+        this.clickAction = null;
 
         this.init();
     };
@@ -31,27 +40,71 @@ class splitexpand {
     };
 
     controls(){
-        this.$list.on("mouseenter", evt=> this.overAct(evt));
+        this.$list.on("click", evt=> {
+            if(!this.opts.clickEvent) return;
+            console.log(this.opts.clickEvent)
+            this.clickAct(evt);
+        });
+        this.$list.on("mouseenter", evt=> {
+            if(!this.clickAction) {
+                this.overAct(evt);
+            }else{
+                this.maxOverAct(evt);
+            }
+        });
         this.$el.on("mouseleave", ()=> this.resetAct());
+    };
+
+    clickAct(evt){
+        this.clickAction = true;
+        this.commAct(evt, "click");
     };
 
     overAct(evt){
         if(!this.overAction) return;
+        this.commAct(evt, "over");
+    };
+
+    maxOverAct(evt){
         $.each(this.$list, (index, element)=>{
             if(element===evt.currentTarget) {
-                $(element).stop().animate({width:this.opts.over}, this.opts.speed);
                 this.currIdx = index;
-                this.$el.trigger("mouseEnter");
-                this.methodDepth("enterCallback");
+                this.$el.trigger("mouseEnterMax");
+                this.methodDepth("enterCallbackMax");
+                this.prevIdx = this.currIdx;
+            }
+        });
+    };
+
+    commAct(evt, str){
+        let strOver, strDown, strMethod, strTrigger;
+        if(str==="click"){
+            strOver = this.opts.maxOver;
+            strDown = this.opts.minDown
+            strTrigger = "mouseClick";
+            strMethod = "clickCallback";
+        }else{
+            strOver = this.opts.over;
+            strDown = this.opts.down
+            strTrigger = "mouseEnter";
+            strMethod = "enterCallback";
+        }
+        $.each(this.$list, (index, element)=>{
+            if(element===evt.currentTarget) {
+                $(element).stop().animate({width:strOver}, this.opts.speed);
+                this.currIdx = index;
+                this.$el.trigger(strTrigger);
+                this.methodDepth(strMethod);
                 this.prevIdx = this.currIdx;
             }else{
-                $(element).stop().animate({width:this.opts.down}, this.opts.speed);
+                $(element).stop().animate({width:strDown}, this.opts.speed);
             };
         });
     };
 
     resetAct(){
         if(!this.overAction) return;
+        this.clickAction = null;
         $.each(this.$list, (index, element)=>{
             this.setStr && $(element).stop().animate({width:this.opts.resetArr[index]}, this.opts.speed);    // this.setStr === true 일 경우 뒤의 값을 반환
             this.setStr || $(element).css({width:this.opts.resetArr[index]});    // this.setStr === false 일 경우 뒤의 값을 반환
