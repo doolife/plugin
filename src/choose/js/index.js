04926d1dbfab98680f2fa00e64cd8604bbfa4e57
doolife <doolife@ncits.co.kr>
2023-12-05 17:53:52 +0900

    [choose] jquery -> es6

--- src/choose/js/index.js
+++ src/choose/js/index.js
@@ -2,18 +2,23 @@ import '../sass/index.scss';
 import Choose from './util/choose';
 
 const choose = new Choose({
-    el:"#contents",
-    data:{
-        tribe:0,
-        job:4,
-        gender:1,
+    el: "#contents",    // 선택자
+    data: {
+        tribe: 1,       // 부족
+        job: 5,         // 직업
+        gender: 1,      // 성별
     },
-    complete:false
+    complete: false,    // 완료 여부
 });
 
-$(".btn__cht").on("click", ()=>{
+// 선택시 콜백 함수
+choose.on("selectCallback", (currTribe, prevJob, currJob, currGender, deviceType) => {
+    console.log(currTribe, prevJob, currJob, currGender, deviceType);
+});
+
+document.querySelector(".btn__cht").addEventListener("click", () => {
     console.log(choose.opts.complete)
-    if(choose.opts.complete) return;
+    if (choose.opts.complete) return;
     console.log(choose.curr.tribe, choose.curr.job, choose.curr.gender)
 });
 
--- src/choose/js/util/choose.js
+++ src/choose/js/util/choose.js
@@ -1,118 +1,164 @@
-class Choose{
-    constructor(opts){
-        this.opts = $.extend(false ,{
-            el:"#element",
-            data:{
-                tribe:1,
-                job:16,
-                gender:0,
+class Choose {
+    constructor(opts) {
+        // 사용자 옵션이 없으면 기본 옵션 사용
+        this.opts = {
+            el: "#element",
+            data: {
+                tribe: 1,
+                job: 16,
+                gender: 0,
             },
-            complete:false
-        }, opts);
+            complete: false,
+            ...opts
+        };
 
-        this.$el = $(this.opts.el);
-        this.$tabWrap = this.$el.find("[data-tab-wrap]");
-        this.$tribeWrap = this.$el.find("[data-tribe-wrap]");
-        this.$genderWrap = this.$el.find("[data-gender-wrap]");
+        // DOM 요소
+        this.el = document.querySelector(this.opts.el);
+        this.tabWrap = this.el.querySelector("[data-tab-wrap]");
+        this.tribeWrap = this.el.querySelector("[data-tribe-wrap]");
+        this.genderWrap = this.el.querySelector("[data-gender-wrap]");
 
-        this.$target = "";
-
+        // 현재 및 이전 상태를 저장하는 객체
         this.curr = {
-            tribe:"",
-            job:"",
-            gender:"",
+            tribe: "",
+            job: "",
+            gender: "",
         };
+        this.prev = { ...this.curr };
 
-        this.prev = {
-            tribe:"",
-            job:"",
-            gender:"",
-        };
-
+        // 모바일 여부
         this.isMobile = navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i);
 
-        this._init();
+        // 콜백 함수를 저장할 객체
+        this.callbacks = {
+            selectCallback: null,
+        };
+
+        // 초기화 함수
+        this.init();
     }
 
-    _init(){
-        this._settings([this.opts.data.tribe, this.opts.data.job, this.opts.data.gender]);
-        this._controls();
+    // 초기 데이터 설정 및 컨트롤 함수
+    init() {
+        const { tribe, job, gender } = this.opts.data;
+        this.settings([tribe, job, gender]);
+        this.controls();
     }
 
-    _controls(){
-        this.$tabWrap.find("[data-tab]").on("click", (evt)=>{
-            if(this.opts.complete) return;
-            let target = $(evt.currentTarget).data("tab");
-            let targetChild = this.$tribeWrap.find(`[data-tribe=${target}]`).find("[data-job]").eq(0).data("job");
-            if(this.curr.tribe === target) return;
+    // 이벤트 리스너를 추가하는 함수
+    addEventListeners(elements, event, callback) {
+        elements.forEach((element) => {
+            element.addEventListener(event, callback);
+        });
+    }
+
+    // 컨트롤 함수
+    controls() {
+        const tabElements = this.tabWrap.querySelectorAll("[data-tab]");
+        this.addEventListeners(tabElements, "click", (evt) => {
+            if (this.opts.complete) return;
+            const target = Number(evt.currentTarget.dataset.tab);
+            const targetChild = Number(this.tribeWrap.querySelector(`[data-tribe="${target}"] [data-job]`).dataset.job);
+            if (this.curr.tribe === target) return;
             this.changeTribe(target);
             this.changeJob(targetChild);
         });
 
-        this.$tribeWrap.find("[data-job-menu]").on("click", (evt)=>{
-            if(this.opts.complete) return;
-            let target = $(evt.currentTarget).data("job-menu");
-            if(this.curr.job === target) return;
+        const jobMenuElements = this.tribeWrap.querySelectorAll("[data-job-menu]");
+        this.addEventListeners(jobMenuElements, "click", (evt) => {
+            if (this.opts.complete) return;
+            const target = Number(evt.currentTarget.dataset.jobMenu);
+            if (this.curr.job === target) return;
             this.changeJob(target);
         });
 
-        this.$genderWrap.find("[data-gender]").on("click", (evt)=>{
-            if(this.opts.complete) return;
-            let target = $(evt.currentTarget).data("gender");
-            if(this.curr.gender === target) return;
+        const genderElements = this.genderWrap.querySelectorAll("[data-gender]");
+        this.addEventListeners(genderElements, "click", (evt) => {
+            if (this.opts.complete) return;
+            const target = Number(evt.currentTarget.dataset.gender);
+            if (this.curr.gender === target) return;
             this.changeGender(target);
         });
     }
 
-    _settings(setArr){
-        this.changeTribe(setArr[0]);
-        this.changeJob(setArr[1]);
-        this.changeGender(setArr[2]);
+    // 초기 데이터 설정
+    settings(setArr) {
+        const [tribe, job, gender] = setArr;
+        this.changeTribe(tribe);
+        this.changeGender(gender);
+        this.changeJob(job);
     }
 
-    changeTribe(tribeStr){
-        this.$tabWrap.find(`[data-tab]`).removeClass("p-tab__list--on");
-        this.$tribeWrap.find(`[data-tribe]`).removeClass("choose__tribe--on");
-        this.$tabWrap.find(`[data-tab=${tribeStr}]`).addClass("p-tab__list--on");
-        this.$tribeWrap.find(`[data-tribe=${tribeStr}]`).addClass("choose__tribe--on");
+    // 부족 변경 함수
+    changeTribe(tribeStr) {
+        this.clearClass(this.tabWrap.querySelectorAll("[data-tab]"), "p-tab__list--on");
+        this.clearClass(this.tribeWrap.querySelectorAll("[data-tribe]"), "choose__tribe--on");
+        this.addClass(this.tabWrap.querySelector(`[data-tab="${tribeStr}"]`), "p-tab__list--on");
+        this.addClass(this.tribeWrap.querySelector(`[data-tribe="${tribeStr}"]`), "choose__tribe--on");
         this.curr.tribe = tribeStr;
     }
 
-    changeJob(jobStr){
-        this.$tribeWrap.find(`[data-job-menu]`).removeClass("job-menu__list--on");
-        this.$tribeWrap.find(`[data-tribe]`).find(`[data-job]`).removeClass("choose__job--on");
-        this.$tribeWrap.find(`[data-job-menu=${jobStr}]`).addClass("job-menu__list--on");
-        this.$tribeWrap.find(`[data-tribe=${this.curr.tribe}]`).find(`[data-job=${jobStr}]`).addClass("choose__job--on");
+    // 직업 변경 함수
+    changeJob(jobStr) {
+        this.clearClass(this.tribeWrap.querySelectorAll("[data-job-menu]"), "job-menu__list--on");
+        this.clearClass(this.tribeWrap.querySelectorAll("[data-tribe] [data-job]"), "choose__job--on");
+        this.addClass(this.tribeWrap.querySelector(`[data-job-menu="${jobStr}"]`), "job-menu__list--on");
+        this.addClass(this.tribeWrap.querySelector(`[data-tribe="${this.curr.tribe}"] [data-job="${jobStr}"]`), "choose__job--on");
         this.curr.job = jobStr;
-        this.checkDevice(jobStr);
+        this.checkDevice();
     }
 
-    changeGender(genderStr){
-        this.$genderWrap.find(`[data-gender]`).removeClass("gender__list--on");
-        this.$genderWrap.find(`[data-gender=${genderStr}]`).addClass("gender__list--on");
+    // 성별 변경 함수
+    changeGender(genderStr) {
+        this.clearClass(this.genderWrap.querySelectorAll("[data-gender]"), "gender__list--on");
+        this.addClass(this.genderWrap.querySelector(`[data-gender="${genderStr}"]`), "gender__list--on");
         this.curr.gender = genderStr;
+        this.checkDevice();
     }
 
-    checkDevice(id){
-        (this.isMobile===null) ? this.jobMovie(id) : this.jobImage(id);
-        this.prev.tribe = this.curr.tribe;
-        this.prev.job = this.curr.job;
-        this.prev.gender = this.curr.gender;
+    // 디바이스 유형 체크
+    checkDevice() {
+        const deviceType = this.isMobile ? "mobile" : "pc";
+        this.selectCallback(deviceType);
+        this.prev = { ...this.curr };
     }
 
-    jobMovie(movieId){
-        console.log(this.curr.tribe, this.prev.job, this.curr.job, `movie__${movieId}`);
+    // web video
+    selectCallback(deviceType) {
+        this.executeCallback("selectCallback", this.curr.tribe, this.prev.job, this.curr.job, this.curr.gender, deviceType);
     }
 
-    jobImage(imgId){
-        console.log(this.curr.tribe, this.prev.job, this.curr.job, `image__${imgId}`);
+    // 외부에서 콜백 등록하는 메서드
+    on(eventName, callback) {
+        this.callbacks[eventName] = callback;
     }
 
-    set complete(setArr){
-        this.changeTribe(setArr[0]);
-        this.changeJob(setArr[1]);
-        this.changeGender(setArr[2]);
-        this.opts.complete = setArr[3];
+    // 콜백 실행 메서드
+    executeCallback(eventName, ...args) {
+        const callback = this.callbacks[eventName];
+        if (callback && typeof callback === "function") {
+            callback(...args);
+        }
+    }
+
+    // 요소들의 클래스를 제거
+    clearClass(elements, className) {
+        elements.forEach((element) => element.classList.remove(className));
+    }
+
+    // 요소에 클래스를 추가
+    addClass(element, className) {
+        if (element) element.classList.add(className);
+    }
+
+    // 완료 상태
+    set complete(setArr) {
+        // 완료 상태 설정
+        const [tribe, job, gender, complete] = setArr;
+        this.changeTribe(tribe);
+        this.changeGender(gender);
+        this.changeJob(job);
+        this.opts.complete = complete;
     }
 }
 
