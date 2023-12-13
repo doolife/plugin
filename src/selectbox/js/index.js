0bbc381be9f27121c6d7796b01c21bb8f1aff8a3
doolife <doolife@ncits.co.kr>
2023-12-13 14:28:48 +0900

    [selectbox] jquery -> es6 & options add

--- src/selectbox/js/index.js
+++ src/selectbox/js/index.js
@@ -2,30 +2,61 @@ import '../sass/index.scss';
 import Selectbox from './page/selectbox';
 
 const selectbox1 = new Selectbox({
-    el:"#select1",
-    height:400,
-    listDisabled:false
-})
+    el: "#select1",
+    items: [
+        {label: '옵션1-1 - 배열', value: [1, 2, 3] },
+        {label: '옵션1-2 - 오브젝트', value: { a: 1, b: 2, c: 3 } },
+        {label: '옵션1-3 - 문자열', value: 'server_04' },
+        {label: '옵션1-4 - 숫자', value: 4 }
+    ],
+    placeholder:'캐릭터를 선택해주세요.1',
+    disabled: false
+});
 
 const selectbox2 = new Selectbox({
     el:"#select2",
-    height:300,
-    listDisabled:false
-})
+    items: [
+        {label: '옵션2-1 - 배열', value: [1, 2, 3]},
+        {label: '옵션2-2 - 오브젝트', value: {"serverid":3,"servername":"서버네임2-2","serveridx":0} },
+        {label: '옵션2-3 - 문자열', value: 'server_03' },
+    ],
+    placeholder:'캐릭터를 선택해주세요.2',
+    disabled: false
+});
 
 const selectbox3 = new Selectbox({
     el:"#select3",
-    height:200,
-    listDisabled:true
-})
-
-for (let i = 0; i < 9; i++) {
-    document.querySelector(".s1").querySelector(".select-box__menu").innerHTML += "<li data-select='list' data-id='"+i+"'><div data-result='text'><i>1_"+i+"</i></div></li>";
-}
-
-for (let i = 0; i < 4; i++) {
-    document.querySelector(".s3").querySelector(".select-box__menu").innerHTML += "<li data-select='list' data-id='"+i+"'><div data-result='text'><i>서버</i><i>캐릭터</i>3_"+i+"</div></li>";
-}
-$("body").on("click", "[data-select=list]", function(){
-    console.log("body!!")
+    items: [
+        {label: '숫자1', value:1},
+        {label: '숫자2', value:2 },
+        {label: '숫자3', value:3 },
+        {label: '숫자4', value:4},
+        {label: '숫자5', value:5},
+        {label: '숫자6', value:6},
+    ],
+    placeholder:'캐릭터를 선택해주세요.3',
+    disabled: false
 });
+
+selectbox1.on("change", () => {
+    console.log(selectbox1.selectedValue, selectbox1.selectedLabel);
+});
+
+selectbox2.on("change", () => {
+    console.log(selectbox2.selectedValue, selectbox2.selectedLabel);
+});
+
+selectbox3.on("change", () => {
+    console.log(selectbox3.selectedValue, selectbox3.selectedLabel);
+});
+
+// selectbox3.update({
+//     selected: 1,
+//     disabled: true,
+//     items: [
+//         {label: '숫자122', value:4},
+//         {label: '숫자2222', value:3 },
+//         {label: '숫자3222', value:2 },
+//         {label: '숫자42222', value:1},
+//     ],
+// });
--- src/selectbox/js/page/selectbox.js
+++ src/selectbox/js/page/selectbox.js
@@ -1,62 +1,198 @@
 class Selectbox {
-    constructor(opts){
-        this.opts = $.extend(true, {
-            el:"#select1",
-            height:400,
-            listDisabled:false
-        }, opts);
+    static instances = [];
 
-        this.$wrap = $(this.opts.el);
-        this.$title = this.$wrap.find("[data-select='title']");
-        this.$menu = this.$wrap.find("[data-select='menu']");
-        this.$list = this.$wrap.find("[data-select='list']");
+    constructor(options) {
+        this.options = {
+            el: "#select1",
+            items: [
+                { label: '옵션1 - 배열', value: [1, 2, 3] },
+                { label: '옵션2 - 오브젝트', value: { a: 1, b: 2, c: 3 } },
+                { label: '옵션3 - 문자열', value: 'server_03' },
+                { label: '옵션4 - 숫자', value: 4 }
+            ],
+            placeholder: '캐릭터를 선택해주세요.2',
+            disabled: false,
+            ...options,
+        };
 
-        this.status;
+        this.wrapper = document.querySelector(this.options.el);
+        this.title = document.createElement("div");
+        this.title.classList.add("select-box__tit");
+        this.title.setAttribute("data-value", "");
+        this.title.textContent = this.options.placeholder;
+        this.wrapper.appendChild(this.title);
+
+        this.menu = document.createElement("ul");
+        this.menu.classList.add("select-box__menu");
+        this.wrapper.appendChild(this.menu);
+
+        this.isOpen = false;
+        this.selectedValue = null;
+        this.selectedLabel = this.options.placeholder;
+        this.itemValues = [];
 
         this.init();
+        Selectbox.instances.push(this);
     }
 
     init() {
-        this.controls();
+        this.renderOptions();
+        this.setupEventListeners();
+        this.updateDisabledState();
     }
 
-    controls(){
-        this.$title.on("click", (evt)=> {this.display(evt)});
-        this.$wrap.on("click", "[data-select='list']", (evt)=> {this.copyAndPaste(evt)});
-        $(document).on("click", (evt)=> {if(this.status===true) this.hide()});
+    setupEventListeners() {
+        this.title.addEventListener("click", (evt) => this.toggleDropdown(evt));
+        this.wrapper.addEventListener("click", (evt) => this.handleItemClick(evt));
+        document.addEventListener("click", () => this.hideDropdown());
     }
 
-    display(evt){
+    static closeOtherSelectboxes(currentInstance) {
+        Selectbox.instances.forEach(instance => {
+            if (instance !== currentInstance && instance.isOpen) {
+                instance.hideDropdown();
+            }
+        });
+    }
+
+    toggleDropdown(evt) {
         evt.stopPropagation();
-        if(this.$wrap.attr("disabled")!==undefined) return;
-        if(!this.$wrap.hasClass("active")){
-            this.hide();
-            this.show();
-        }else{
-            this.hide();
+        if (this.isDisabled()) return;
+        Selectbox.closeOtherSelectboxes(this);
+        this.isOpen ? this.hideDropdown() : this.showDropdown();
+    }
+
+    showDropdown() {
+        this.wrapper.classList.add("active");
+        this.isOpen = true;
+    }
+
+    hideDropdown() {
+        this.wrapper.classList.remove("active");
+        this.isOpen = false;
+    }
+
+    handleItemClick(evt) {
+        if (evt.target.classList.contains("select-box__list")) {
+            const label = evt.target.textContent;
+            const dataId = evt.target.getAttribute("data-id");
+            const jsonString = evt.target.getAttribute("data-value");
+
+            this.title.textContent = label;
+            this.title.setAttribute("data-value", jsonString);
+
+            this.selectedValue = this.itemValues[dataId];
+            this.selectedLabel = label;
+
+            this.triggerChange();
+            this.hideDropdown();
         }
     }
 
-    show(){
-        this.$wrap.addClass("active");
-        this.$menu.css({height:this.opts.height, overflowY:"auto"});
-        this.status = true;
+    renderOptions() {
+        this.options.items.forEach((item, idx) => {
+            const li = this.createListItem(item, idx);
+            this.menu.appendChild(li);
+        });
     }
 
-    hide(){
-        $("[data-select='wrap']").removeClass("active");
-        this.$menu.css({height:"", overflowY:""});
-        this.status = false;
+    createListItem(item, idx) {
+        const li = document.createElement("li");
+        const jsonString = JSON.stringify(item.value);
+
+        this.itemValues.push(item.value);
+
+        li.classList.add("select-box__list");
+        li.setAttribute("data-id", idx);
+        li.setAttribute("data-value", jsonString.replace(/\"/g, ''));
+        li.textContent = item.label;
+
+        return li;
     }
 
-    copyAndPaste(evt){
-        if(this.opts.listDisabled) {
-            evt.stopPropagation();
-            return;
+    update(options) {
+        if (options.selected !== undefined) {
+            this.updateSelected(options.selected, options.items);
         }
-        this.hide();
-        let clones = $(evt.currentTarget).find("[data-result]").clone();
-        this.$title.empty().append(clones);
+        if (options.disabled !== undefined) {
+            this.options.disabled = options.disabled;
+            this.updateDisabledState();
+        }
+        if (options.items !== undefined) {
+            this.updateItems(options.items);
+        }
+        if (options.placeholder !== undefined) {
+            this.updatePlaceholder(options.placeholder);
+        }
+    }
+
+    updateSelected(selectedValue, items) {
+        console.log(items);  // 이 부분을 통해 selectedValue 값 확인
+        const selectedIdx = this.itemValues.findIndex(itemValue => JSON.stringify(itemValue) === JSON.stringify(selectedValue));
+
+        if (selectedIdx !== -1) {
+            const returnTitle = items.find(item => item.value === selectedValue);
+            const selectedLabel = this.options.items[selectedIdx].label;
+            this.updateTitleAndValue(returnTitle.label, selectedValue);
+            this.updateSelectedStyle(selectedIdx);
+        }
+    }
+
+    updateTitleAndValue(label, value) {
+        this.title.textContent = label;
+        this.title.setAttribute("data-value", JSON.stringify(value));
+        this.selectedValue = value;
+        this.selectedLabel = label;
+    }
+
+    updateSelectedStyle(selectedIdx) {
+        const selectedListItem = this.menu.querySelector(`[data-id="${selectedIdx}"]`);
+        if (selectedListItem) {
+            this.menu.querySelectorAll(".select-box__list").forEach(item => {
+                item.classList.remove("selected");
+            });
+            selectedListItem.classList.add("selected");
+        }
+    }
+
+    updateDisabledState() {
+        if (this.isDisabled()) {
+            this.wrapper.setAttribute("disabled", "");
+        } else {
+            this.wrapper.removeAttribute("disabled");
+        }
+    }
+
+    isDisabled() {
+        return this.options.disabled || this.disabled;
+    }
+
+    updateItems(items) {
+        this.menu.innerHTML = "";
+        this.itemValues = [];
+        items.forEach((item, idx) => {
+            const li = this.createListItem(item, idx);
+            this.menu.appendChild(li);
+        });
+    }
+
+    updatePlaceholder(placeholder) {
+        this.options.placeholder = placeholder;
+        this.title.textContent = placeholder;
+    }
+
+    on(eventName, callback) {
+        this.eventHandlers = this.eventHandlers || {};
+        this.eventHandlers[eventName] = this.eventHandlers[eventName] || [];
+        this.eventHandlers[eventName].push(callback);
+    }
+
+    triggerChange() {
+        if (this.eventHandlers && this.eventHandlers["change"]) {
+            this.eventHandlers["change"].forEach(callback => {
+                callback();
+            });
+        }
     }
 }
 
